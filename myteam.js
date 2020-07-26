a19e6a9f60cbe433d0bf15451c505603c2d78e12
Mathew Poovathany <poovathanymathew@gmail.com>
2020-07-26 15:48:13 -0400

    Initial modifications

--- myteam.js
+++ myteam.js
@@ -5,8 +5,8 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "index.html");
+//const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join("index.html");
 
 const render = require("./lib/htmlRenderer");
 
